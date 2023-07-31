f766a675e34664e63300dc39d04e9b8cf26c8081
Derek <derekdanielson84@gmail.com>
2023-07-31 10:04:21 -0600

    Added list of people

---
+++ app.js
@@ -0,0 +1,31 @@
+// Local reviews data
+const reviews = [
+    {
+        id: 1,
+        name: "Bob Bobson",
+        job: "ux designer",
+        img: "./person1.png",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ut repellat facilis, odio excepturi assumenda culpa veniam impedit quae quibusdam corrupti est. Consequatur velit odit animi perferendis harum maxime tempora."
+    },
+    {
+        id: 2,
+        name: "John Doe",
+        job: "web developer",
+        img: "./person2.png",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ut repellat facilis, odio excepturi assumenda culpa veniam impedit quae quibusdam corrupti est. Consequatur velit odit animi perferendis harum maxime tempora."
+    },
+    {
+        id: 3,
+        name: "Susan Swanson",
+        job: "ux designer",
+        img: "./person3.png",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ut repellat facilis, odio excepturi assumenda culpa veniam impedit quae quibusdam corrupti est. Consequatur velit odit animi perferendis harum maxime tempora."
+    },
+    {
+        id: 4,
+        name: "Steve Simpson",
+        job: "web developer",
+        img: "./person4.png",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ut repellat facilis, odio excepturi assumenda culpa veniam impedit quae quibusdam corrupti est. Consequatur velit odit animi perferendis harum maxime tempora."
+    }
+];
