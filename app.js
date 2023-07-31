9118b5110cbeb9e9ede1201dc76c186d5ccd4144
Derek <derekdanielson84@gmail.com>
2023-07-31 11:07:53 -0600

    Added query selectors

--- app.js
+++ app.js
@@ -29,3 +29,20 @@ const reviews = [
         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ut repellat facilis, odio excepturi assumenda culpa veniam impedit quae quibusdam corrupti est. Consequatur velit odit animi perferendis harum maxime tempora."
     }
 ];
+
+
+// Select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+
+// Set starting item
+let currentItem = 0;
+
+
